c1499c7bf4095f76bcc93a767dc454ba969c2bdb
Pedro Kauã <pk.bleite@gmail.com>
2023-05-07 12:13:25 -0300

    rockPaperScissors

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -19,25 +19,25 @@ function playerChoice(playerChoosed){
         computerChoice = Math.floor(Math.random() * (3)+1);
 
         if(playerChoosed == "rock"){
-            document.querySelector("#secondImage").src="/RPS/images/rock2.png";
+            document.querySelector("#secondImage").src="./images/rock2.png";
         }else if(playerChoosed == "paper"){
-            document.querySelector("#secondImage").src="/RPS/images/Paper.png";
+            document.querySelector("#secondImage").src="./images/Paper.png";
         }else{
-            document.querySelector("#secondImage").src="/RPS/images/Scissors.png";
+            document.querySelector("#secondImage").src="./images/Scissors.png";
         }
         
         if(computerChoice == 1){
-            document.querySelector("#firstImage").src="/RPS/images/rock2.png";
+            document.querySelector("#firstImage").src="./images/rock2.png";
         }else if(computerChoice == 2){
-            document.querySelector("#firstImage").src="/RPS/images/Paper.png";
+            document.querySelector("#firstImage").src="./images/Paper.png";
         }else{
-            document.querySelector("#firstImage").src="/RPS/images/Scissors.png";
+            document.querySelector("#firstImage").src="./images/Scissors.png";
         }
         if(winningC == roundsGame || winningP == roundsGame){
             if(winningC > winningP){
-                alert("Computer Wow! Death to all the organic beings! But before that, lets play again.Pick your choice");
+                alert("Computer won! Death to all the organic beings!");
             }else{
-                alert("Player won... For now. If he wants to challenge the computer again, he shall choose!");
+                alert("Player won... For now.");
             }
             winningC = 0;
             winningP = 0;
@@ -48,7 +48,7 @@ function playerChoice(playerChoosed){
         {
             winningP++;
             document.querySelector(".whoWon").innerHTML = "Player Won!";
-            document.querySelector(".gameScore").innerHTML = `${winningC}||${winningP}`
+            document.querySelector(".gameScore").innerHTML = `${winningC} || ${winningP}`;
             return playerChoosed;
             
         
@@ -58,14 +58,16 @@ function playerChoice(playerChoosed){
         else if(computerChoice == 1 && playerChoosed == 'rock' || computerChoice == 2 && playerChoosed == 'paper' || computerChoice == 3 && playerChoosed == 'scissors'){
             
             document.querySelector(".whoWon").innerHTML = "It's a tie. Both choosed the same.";
-            document.querySelector(".gameScore").innerHTML = `${winningC}||${winningP}`
+            document.querySelector(".gameScore").innerHTML = `${winningC} || ${winningP}`
+
             return playerChoosed
             
         }
         else{
             winningC++;
             document.querySelector(".whoWon").innerHTML = "Player lost this round."
-            document.querySelector(".gameScore").innerHTML = `${winningC}||${winningP}`
+            document.querySelector(".gameScore").textContent = `${winningC} || ${winningP}`;
+            
             return computerChoice;
             
         }
